b5a5826009fc74a52ed414d77af7bb9ec5d30e4e
ajinkyajagdale21 <ajinkyajagdale21@gmail.com>
2022-06-10 13:01:35 +0530

    update: minor changes

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,65 +1,69 @@
-let id=0;
-export const projects=[
-    {
-        
-        pid:++id,
-        image: "https://cdn.pixabay.com/photo/2018/03/14/01/03/subscribe-3224137_960_720.jpg",
-        projectName:"SwiftFlix",
-        ProjectCategorie:"VideoLibrary App",
-        ProjectDetails:"VideoLibrary Web App Made Using",
-        liveLink:"https://swiftflix.netlify.app/",
-        repo: "https://github.com/ajinkyajagdale21/video-player/tree/dev",
-        TechStack: ["ReactJs","MongoDB","ExpressJS","NodeJS"]
-    },
-    {
-        pid:++id,
-        image: "https://cdn.pixabay.com/photo/2017/01/19/13/22/ecommerce-1992280_960_720.png",
-        projectName:"PeachCart App",
-        ProjectCategorie:"Ecommerce",
-        liveLink:"https://peachcart21.netlify.app/",
-        repo:"https://github.com/ajinkyajagdale21/peachcart/tree/development",
-        ProjectDetails:"Ecommerce Web App Made Using",
-        TechStack: ["ReactJs","MongoDB","ExpressJS","NodeJS"]
-    },
-    {
-        pid:++id,
-        image: "https://cdn.pixabay.com/photo/2016/11/19/23/00/css3-1841590_960_720.png",
-        projectName:"PeachCSS",
-        ProjectCategorie:"Component Library",
-        liveLink:"https://peachcss.netlify.app/",
-        repo:"https://github.com/ajinkyajagdale21/PeachCSS-Component-Library",
-        ProjectDetails:"ComponentLibrary Made Using",
-        TechStack: ["Vanilla CSS"]
-    },
-    {
-        pid:++id,
-        image: "https://cdn.pixabay.com/photo/2015/01/20/13/13/samsung-605439__340.jpg",
-        projectName:"Spotunes",
-        ProjectCategorie:"Music App",
-        liveLink:"https://aji21-music-player.netlify.app/",
-        repo:"https://github.com/ajinkyajagdale21/music-player",
-        ProjectDetails:"Music App Made Using",
-        TechStack: ["ReactJS"]
-    },
-    {
-        pid:++id,
-        image: "https://cdn.pixabay.com/photo/2015/03/26/09/40/keyboard-690066__340.jpg",
-        projectName:"Reaction game",
-        ProjectCategorie:"Web Game",
-        liveLink:"https://aj21-reaction-game.netlify.app/",
-        repo:"https://github.com/ajinkyajagdale21/reaction-game",
-        ProjectDetails:"Reaction game webApp Made Using",
-        TechStack: ["ReactJS"]
-    },
-    {
-        pid:++id,
-        image: "https://cdn.pixabay.com/photo/2014/07/06/13/55/calculator-385506__480.jpg",
-        projectName:"Calculate IT",
-        ProjectCategorie:"Calculator app",
-        liveLink:"https://aj21-calculator.netlify.app/",
-        repo:"https://github.com/ajinkyajagdale21/calculator/tree/main/src",
-        ProjectDetails:"calculator webApp Made Using",
-        TechStack: ["ReactJS"]
-    },
-
-]
+let id = 0;
+export const projects = [
+  {
+    pid: ++id,
+    image:
+      "https://cdn.pixabay.com/photo/2018/03/14/01/03/subscribe-3224137_960_720.jpg",
+    projectName: "SwiftFlix",
+    ProjectCategorie: "VideoLibrary App",
+    ProjectDetails: "VideoLibrary Web App Made Using",
+    liveLink: "https://swiftflix.netlify.app/",
+    repo: "https://github.com/ajinkyajagdale21/video-player/tree/dev",
+    TechStack: ["ReactJs", "MongoDB", "ExpressJS", "NodeJS"],
+  },
+  {
+    pid: ++id,
+    image:
+      "https://cdn.pixabay.com/photo/2017/01/19/13/22/ecommerce-1992280_960_720.png",
+    projectName: "PeachCart App",
+    ProjectCategorie: "Ecommerce",
+    liveLink: "https://peachcart21.netlify.app/",
+    repo: "https://github.com/ajinkyajagdale21/peachcart/tree/development",
+    ProjectDetails: "Ecommerce Web App Made Using",
+    TechStack: ["ReactJs", "MongoDB", "ExpressJS", "NodeJS"],
+  },
+  {
+    pid: ++id,
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/19/23/00/css3-1841590_960_720.png",
+    projectName: "PeachCSS",
+    ProjectCategorie: "Component Library",
+    liveLink: "https://peachcss.netlify.app/",
+    repo: "https://github.com/ajinkyajagdale21/PeachCSS-Component-Library",
+    ProjectDetails: "ComponentLibrary Made Using",
+    TechStack: ["Vanilla CSS"],
+  },
+  {
+    pid: ++id,
+    image:
+      "https://cdn.pixabay.com/photo/2015/01/20/13/13/samsung-605439__340.jpg",
+    projectName: "Spotunes",
+    ProjectCategorie: "Music App",
+    liveLink: "https://aji21-music-player.netlify.app/",
+    repo: "https://github.com/ajinkyajagdale21/music-player",
+    ProjectDetails: "Music App Made Using",
+    TechStack: ["ReactJS"],
+  },
+  {
+    pid: ++id,
+    image:
+      "https://cdn.pixabay.com/photo/2022/01/10/12/33/balance-6928218_960_720.png",
+    projectName: "Reaction game",
+    ProjectCategorie: "Web Game",
+    liveLink: "https://aj21-reaction-game.netlify.app/",
+    repo: "https://github.com/ajinkyajagdale21/reaction-game",
+    ProjectDetails: "Reaction game webApp Made Using",
+    TechStack: ["ReactJS"],
+  },
+  {
+    pid: ++id,
+    image:
+      "https://cdn.pixabay.com/photo/2014/07/06/13/55/calculator-385506__480.jpg",
+    projectName: "Calculate IT",
+    ProjectCategorie: "Calculator app",
+    liveLink: "https://aj21-calculator.netlify.app/",
+    repo: "https://github.com/ajinkyajagdale21/calculator/tree/main/src",
+    ProjectDetails: "calculator webApp Made Using",
+    TechStack: ["ReactJS"],
+  },
+];
